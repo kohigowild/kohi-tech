c9ac2857140ab7f5de41d5ad630392aad95391b6
kohi <mainmirror14@gmail.com>
2024-07-08 19:42:27 +0900

    fix : 페이지네이션 라이브러리 교체

--- components/index/ListPage.tsx
+++ components/index/ListPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Pagination from 'react-js-pagination'
+import Pagination from 'rc-pagination'
 
 interface Props {
   page: number
@@ -13,12 +13,12 @@ const ListPage = (props: Props) => {
 
   return (
     <Pagination
-      activePage={page} // 현재 페이지
-      itemsCountPerPage={itemsCountPerPage} // 한 페이지랑 보여줄 아이템 갯수
-      totalItemsCount={totalItemsCount} // 총 아이템 갯수
-      pageRangeDisplayed={5} // paginator의 페이지 범위
-      prevPageText={'‹'} // "이전"을 나타낼 텍스트
-      nextPageText={'›'} // "다음"을 나타낼 텍스트
+      current={page} // 현재 페이지
+      defaultPageSize={itemsCountPerPage} // 한 페이지랑 보여줄 아이템 갯수
+      total={totalItemsCount} // 총 아이템 갯수
+      pageSize={5} // paginator의 페이지 범위
+      prevIcon={'‹'} // "이전"을 나타낼 텍스트
+      nextIcon={'›'} // "다음"을 나타낼 텍스트
       onChange={handlePageChange} // 페이지 변경을 핸들링하는 함수
     />
   )
