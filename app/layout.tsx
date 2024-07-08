8b886e0d40e9ae5f755eb6ddad009ec1758f459a
kohi <mainmirror14@gmail.com>
2024-07-08 20:04:48 +0900

    update : 구글 애널리틱스 추적 코드 삽입

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 import RecoilProvider from '@/components/common/RecoilProvider'
 import QueryProvider from '@/components/common/QueryProvider'
 import ToastProvider from '@/components/common/ToastProvider'
+import AnalyticsProvider from '@/components/common/AnalyticsProvider'
 import Suspense from '@/components/common/Suspense'
 import Header from '@/components/common/Header'
 import Loading from '@/components/common/Loading'
@@ -26,12 +28,32 @@ export default function RootLayout({
       <RecoilProvider>
         <QueryProvider>
           <Suspense>
-            <body className={inter.className}>
-              <Header />
-              <Loading />
-              {children}
-              <ToastProvider />
-            </body>
+            <Script
+              strategy='afterInteractive'
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+            />
+            <Script
+              id='gtag-init'
+              strategy='afterInteractive'
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+              }}
+            />
+            <AnalyticsProvider>
+              <body className={inter.className}>
+                <Header />
+                <Loading />
+                {children}
+                <ToastProvider />
+              </body>
+            </AnalyticsProvider>
           </Suspense>
         </QueryProvider>
       </RecoilProvider>
