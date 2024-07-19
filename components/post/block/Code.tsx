38557ef485f96ff474e6723116085ba21c986ceb
kohi <mainmirror14@gmail.com>
2024-07-19 22:46:27 +0900

    fix : 코드 블록 오타 수정

--- components/post/block/Code.tsx
+++ components/post/block/Code.tsx
@@ -8,8 +8,8 @@ export default function Code(props: Props) {
   const { parent } = props
   const string = parent.replace(/^```([\s\S]*)```$/, '$1')
 
-  const lang = parent.split('\n')[0]
-  const code = parent.split('\n').slice(1).join('\n')
+  const lang = string.split('\n')[0]
+  const code = string.split('\n').slice(1).join('\n')
   return (
     <div className='max-w-3xl mx-auto mt-8'>
       <pre className='bg-gray-800 rounded-lg p-6'>
